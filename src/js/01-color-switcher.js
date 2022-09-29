cc49c3e17ef7d233fd3703c43cde5263da482ef0
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2022-09-29 17:44:53 +0300

    add ch to 01-switch-color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,9 +4,9 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-bodyRef = document.querySelector('body');
-btnStart = document.querySelector('[data-start]');
-btnStop = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
 console.log('btnStart', btnStart);
 console.log('btnStop', btnStop);
